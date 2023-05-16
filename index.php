b046ae9f7d5d66dfbdd3da6588e030981fbbdf19
Paul Sia <2149093@talnet.nl>
2023-05-16 08:51:39 +0200

    opps

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$var1 = 12;
+$var2 = 10;
+$sum = $var1 + $var2;
+echo $sum;
+?>
+
+<?php
+$var1 = 12;
+$var2 = 10;
+$product = $var1 * $var2;
+echo $product;
+?>
+
+<?php
+$naam = "Jouw naam hier";
+echo "Welkom " . $naam . "!";
+?>
+
+<?php
+$mijnNaam = "Carl";
+$jouwNaam = "Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+?>
+  
+</body>
+</html>
